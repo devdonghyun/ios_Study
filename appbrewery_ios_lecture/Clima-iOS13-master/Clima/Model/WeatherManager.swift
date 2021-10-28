5e4d579161650a0ed6120f121c49601c596eaaeb
devDongHYun <ehdgus8725@naver.com>
2021-10-28 17:39:17 +0900

    Add Getting Location by Coordinates Function & Refactor with Delegate Pattern + Extensions

--- appbrewery_ios_lecture/Clima-iOS13-master/Clima/Model/WeatherManager.swift
+++ appbrewery_ios_lecture/Clima-iOS13-master/Clima/Model/WeatherManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 
 protocol WeatherManagerDelegate {
@@ -22,6 +23,11 @@ struct WeatherManager {
     
     func fetchWeater(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
         performRequest(with: urlString)
     }
     
