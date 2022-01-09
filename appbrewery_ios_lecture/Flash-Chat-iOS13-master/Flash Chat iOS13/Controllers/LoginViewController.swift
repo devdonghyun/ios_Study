d6037a218dc7fbbc7d2c6cc13b8d6b3a59d369c7
devDongHYun <ehdgus8725@naver.com>
2022-01-09 16:18:41 +0900

    Add Firebase Authentication

--- appbrewery_ios_lecture/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/LoginViewController.swift
+++ appbrewery_ios_lecture/Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -15,14 +16,19 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func loginPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "goToTableView", sender: self)
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+              
+            }
+        }
         
     }
+
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goToTableView" {
-            let destinationVC = segue.destination as! ChatViewController
-        }
-    }
     
 }
