c8d401346d6ac7ef4dea2072ba6733239f423f49
devDongHYun <ehdgus8725@naver.com>
2022-01-15 20:10:56 +0900

    Add Messagebubble with .xib

---
+++ appbrewery_ios_lecture/Flash-Chat-iOS13-master/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 안동현 on 2022/01/15.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // 1@2.com
+    let body: String
+}
